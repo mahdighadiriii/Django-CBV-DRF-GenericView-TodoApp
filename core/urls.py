a4d04ce4518d693cc573957f9f99b9effb41242d
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-10-16 19:51:13 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponseRedirect
+
+
+def redirectToApp(request):
+    return HttpResponseRedirect("/todo")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", redirectToApp, name="index"),
+    path("todo/", include("todo.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
